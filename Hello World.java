b009a83474398b5db41c37edf7f85791e8a0cd54
leelove6045 <62539560+leelove6045@users.noreply.github.com>
2020-03-24 00:39:11 +0900

    Create Hello World.java

---
+++ Hello World.java
@@ -0,0 +1,11 @@
+package 연습1;
+
+public class practice {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, World!");
+		System.out.println("I am a new Java programmer!");
+	}
+
+}
