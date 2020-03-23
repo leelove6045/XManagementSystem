9d4d10f6bbab628648d0e1ee39bd360aae79a4a7
user <leelove6045@gmail.com>
2020-03-24 04:11:17 +0900

    add menu selection
    Signed-off-by: user <leelove6045@gmail.com>

--- src/Hello World.java
+++ src/Hello World.java
@@ -1,11 +1,13 @@
-package 연습1;
 
 public class practice {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Hello, World!");
-		System.out.println("I am a new Java programmer!");
+		System.out.println("Select one number between 1 - 5 !");
+		System.out.println("1. Add Student");
+		System.out.println("2. Delete Student");
+		System.out.println("3. Edit Student");
+		System.out.println("4. View Student");
+		System.out.println("5. Exit");
 	}
 
 }
