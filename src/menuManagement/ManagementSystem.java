e8fc1f869a6c62e5f449f0d61de1bb89e59a281b
user <user@LAPTOP-6OGVEV2U>
2020-03-30 17:45:54 +0900

    management system

--- src/menuManagement/ManagementSystem.java
+++ src/menuManagement/ManagementSystem.java
@@ -1,10 +1,65 @@
 package menuManagement;
 
+import java.util.Scanner;
+
 public class ManagementSystem {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		int num = 5;
+		
+		while (num != 6) {
+			System.out.println("*** Student Management System Menu ***");
+			System.out.println("1. Add Student");
+			System.out.println("2. Delete Student");
+			System.out.println("3. Edit Student");
+			System.out.println("4. View Student");
+			System.out.println("5. Show menu");
+			System.out.println("6. Exit");
+			System.out.println("Select one number between 1 - 5:");
+			num = input .nextInt();
+			if (num==1) {
+				addStudent();
+			}
+			else if (num==2) {
+				deleteStudent();
+			}
+			else if (num==3) {
+				editStudent();
+			}
+			else if (num==4) {
+				viewStudent();
+			}
+			else {
+				continue;
+			}
+		}
 	}
-
+	public static void addStudent() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Student ID :");
+		int studentId = input.nextInt();
+		System.out.print("Student name :");
+		String studentname = input.next();
+		System.out.print("Email address :");
+		String studentEmail = input.next();
+		System.out.print("Phone number :");
+		String studentPhone = input.next();
+		
+	}
+	public static void deleteStudent() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Student ID :");
+		int studentId = input.nextInt();	
+	}
+	public static void editStudent() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Student ID :");
+		int studentId = input.nextInt();
+	}
+	public static void viewStudent() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Student ID :");
+		int studentId = input.nextInt();
+	}
 }
